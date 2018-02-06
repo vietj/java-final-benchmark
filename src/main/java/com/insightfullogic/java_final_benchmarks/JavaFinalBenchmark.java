453d6fa7dc66ceb3a2d40df41634e45641cb8e5c
Julien Viet <julien@julienviet.com>
2018-02-06 21:49:17 +0100

    Edits

--- src/main/java/com/insightfullogic/java_final_benchmarks/JavaFinalBenchmark.java
+++ src/main/java/com/insightfullogic/java_final_benchmarks/JavaFinalBenchmark.java
@@ -20,259 +20,259 @@ import static org.openjdk.jmh.annotations.Scope.Thread;
 @State(Thread)
 public class JavaFinalBenchmark {
 
-    // Deliberately a field, JMH avoids constant folding
-    private double x = Math.PI;
+  // Deliberately a field, JMH avoids constant folding
+  private double x = Math.PI;
 
-    private TargetClass1 target;
-    private InlinableTargetClass1 inlinableTarget;
+  private TargetClass1 target;
+  private InlinableTargetClass1 inlinableTarget;
 
-    @Setup
-    public void setup() {
-        target = new TargetClass1();
-        inlinableTarget = new InlinableTargetClass1();
+  @Setup
+  public void setup() {
+    target = new TargetClass1();
+    inlinableTarget = new InlinableTargetClass1();
+  }
+
+  @Benchmark
+  public void virtualInvoke() {
+    target.targetVirtual();
+  }
+
+  @Benchmark
+  public void finalInvoke() {
+    target.targetFinal();
+  }
+
+  @Benchmark
+  public void inlinableVirtualInvoke() {
+    inlinableTarget.targetVirtual();
+  }
+
+  @Benchmark
+  public void inlinableFinalInvoke() {
+    inlinableTarget.targetFinal();
+  }
+
+  /**
+   * Inherited Methods
+   * <p/>
+   * Test the hypothesis of distance up the class hierarchy affects the invoke performance.
+   * Numbers refer to how far up the class hierarchy the inherited method is from
+   */
+
+  @Benchmark
+  public void parentMethod1() {
+    target.inheritedTarget1();
+  }
+
+  @Benchmark
+  public void parentMethod2() {
+    target.inheritedTarget2();
+  }
+
+  @Benchmark
+  public void parentMethod3() {
+    target.inheritedTarget3();
+  }
+
+  @Benchmark
+  public void parentMethod4() {
+    target.inheritedTarget4();
+  }
+
+  @Benchmark
+  public void parentFinalMethod1() {
+    target.inheritedFinalTarget1();
+  }
+
+  @Benchmark
+  public void parentFinalMethod2() {
+    target.inheritedFinalTarget2();
+  }
+
+  @Benchmark
+  public void parentFinalMethod3() {
+    target.inheritedFinalTarget3();
+  }
+
+  @Benchmark
+  public void parentFinalMethod4() {
+    target.inheritedFinalTarget4();
+  }
+
+  @Benchmark
+  public void alwaysOverriddenMethod() {
+    target.alwaysOverriddenTarget();
+  }
+
+  @Benchmark
+  public double inlinableParentMethod1() {
+    return inlinableTarget.inheritedTarget1();
+  }
+
+  @Benchmark
+  public double inlinableParentMethod2() {
+    return inlinableTarget.inheritedTarget2();
+  }
+
+  @Benchmark
+  public double inlinableParentMethod3() {
+    return inlinableTarget.inheritedTarget3();
+  }
+
+  @Benchmark
+  public double inlinableParentMethod4() {
+    return inlinableTarget.inheritedTarget4();
+  }
+
+  @Benchmark
+  public double inlinableParentFinalMethod1() {
+    return inlinableTarget.inheritedFinalTarget1();
+  }
+
+  @Benchmark
+  public double inlinableParentFinalMethod2() {
+    return inlinableTarget.inheritedFinalTarget2();
+  }
+
+  @Benchmark
+  public double inlinableParentFinalMethod3() {
+    return inlinableTarget.inheritedFinalTarget3();
+  }
+
+  @Benchmark
+  public double inlinableParentFinalMethod4() {
+    return inlinableTarget.inheritedFinalTarget4();
+  }
+
+  @Benchmark
+  public double inlinableAlwaysOverriddenMethod() {
+    return inlinableTarget.alwaysOverriddenTarget();
+  }
+
+
+  public class InlinableTargetClass1 extends InlinableTargetClass2 {
+    public double alwaysOverriddenTarget() {
+      return x;
     }
 
-    @Benchmark
-    public void virtualInvoke() {
-        target.targetVirtual();
+    public double inheritedTarget1() {
+      return x;
     }
 
-    @Benchmark
-    public void finalInvoke() {
-        target.targetFinal();
+    public final double inheritedFinalTarget1() {
+      return x;
     }
 
-    @Benchmark
-    public void inlinableVirtualInvoke() {
-        inlinableTarget.targetVirtual();
+    public double targetVirtual() {
+      return x;
     }
 
-    @Benchmark
-    public void inlinableFinalInvoke() {
-        inlinableTarget.targetFinal();
+    public final double targetFinal() {
+      return x;
+    }
+  }
+
+  public class InlinableTargetClass2 extends InlinableTargetClass3 {
+    public double alwaysOverriddenTarget() {
+      return x;
     }
 
-    /**
-     * Inherited Methods
-     * <p/>
-     * Test the hypothesis of distance up the class hierarchy affects the invoke performance.
-     * Numbers refer to how far up the class hierarchy the inherited method is from
-     */
-
-    @Benchmark
-    public void parentMethod1() {
-        target.inheritedTarget1();
+    public double inheritedTarget2() {
+      return x;
     }
 
-    @Benchmark
-    public void parentMethod2() {
-        target.inheritedTarget2();
+    public final double inheritedFinalTarget2() {
+      return x;
+    }
+  }
+
+  public class InlinableTargetClass3 extends InlinableTargetClass4 {
+    public double alwaysOverriddenTarget() {
+      return x;
     }
 
-    @Benchmark
-    public void parentMethod3() {
-        target.inheritedTarget3();
+    public double inheritedTarget3() {
+      return x;
     }
 
-    @Benchmark
-    public void parentMethod4() {
-        target.inheritedTarget4();
+    public final double inheritedFinalTarget3() {
+      return x;
+    }
+  }
+
+  public class InlinableTargetClass4 {
+    public double alwaysOverriddenTarget() {
+      return x;
     }
 
-    @Benchmark
-    public void parentFinalMethod1() {
-        target.inheritedFinalTarget1();
+    public double inheritedTarget4() {
+      return x;
     }
 
-    @Benchmark
-    public void parentFinalMethod2() {
-        target.inheritedFinalTarget2();
+    public final double inheritedFinalTarget4() {
+      return x;
+    }
+  }
+
+  @CompilerControl(DONT_INLINE)
+  public static class TargetClass1 extends TargetClass2 {
+    public void alwaysOverriddenTarget() {
     }
 
-    @Benchmark
-    public void parentFinalMethod3() {
-        target.inheritedFinalTarget3();
+    public void inheritedTarget1() {
     }
 
-    @Benchmark
-    public void parentFinalMethod4() {
-        target.inheritedFinalTarget4();
+    public final void inheritedFinalTarget1() {
     }
 
-    @Benchmark
-    public void alwaysOverriddenMethod() {
-        target.alwaysOverriddenTarget();
+    public void targetVirtual() {
     }
 
-    @Benchmark
-    public double inlinableParentMethod1() {
-        return inlinableTarget.inheritedTarget1();
+    public final void targetFinal() {
+    }
+  }
+
+  @CompilerControl(DONT_INLINE)
+  public static class TargetClass2 extends TargetClass3 {
+    public void alwaysOverriddenTarget() {
     }
 
-    @Benchmark
-    public double inlinableParentMethod2() {
-        return inlinableTarget.inheritedTarget2();
+    public void inheritedTarget2() {
     }
 
-    @Benchmark
-    public double inlinableParentMethod3() {
-        return inlinableTarget.inheritedTarget3();
+    public final void inheritedFinalTarget2() {
+    }
+  }
+
+  @CompilerControl(DONT_INLINE)
+  public static class TargetClass3 extends TargetClass {
+    public void alwaysOverriddenTarget() {
     }
 
-    @Benchmark
-    public double inlinableParentMethod4() {
-        return inlinableTarget.inheritedTarget4();
+    public void inheritedTarget3() {
     }
 
-    @Benchmark
-    public double inlinableParentFinalMethod1() {
-        return inlinableTarget.inheritedFinalTarget1();
+    public final void inheritedFinalTarget3() {
+    }
+  }
+
+  @CompilerControl(DONT_INLINE)
+  public static class TargetClass {
+    public void alwaysOverriddenTarget() {
     }
 
-    @Benchmark
-    public double inlinableParentFinalMethod2() {
-        return inlinableTarget.inheritedFinalTarget2();
+    public void inheritedTarget4() {
     }
 
-    @Benchmark
-    public double inlinableParentFinalMethod3() {
-        return inlinableTarget.inheritedFinalTarget3();
+    public final void inheritedFinalTarget4() {
     }
+  }
 
-    @Benchmark
-    public double inlinableParentFinalMethod4() {
-        return inlinableTarget.inheritedFinalTarget4();
-    }
+  public static void main(String[] args) throws RunnerException {
+    Options opt = new OptionsBuilder()
+        .include(".*" + JavaFinalBenchmark.class.getSimpleName() + ".*")
+        .build();
 
-    @Benchmark
-    public double inlinableAlwaysOverriddenMethod() {
-        return inlinableTarget.alwaysOverriddenTarget();
-    }
-
-
-    public class InlinableTargetClass1 extends InlinableTargetClass2 {
-        public double alwaysOverriddenTarget() {
-            return x;
-        }
-
-        public double inheritedTarget1() {
-            return x;
-        }
-
-        public final double inheritedFinalTarget1() {
-            return x;
-        }
-
-        public double targetVirtual() {
-            return x;
-        }
-
-        public final double targetFinal() {
-            return x;
-        }
-    }
-
-    public class InlinableTargetClass2 extends InlinableTargetClass3 {
-        public double alwaysOverriddenTarget() {
-            return x;
-        }
-
-        public double inheritedTarget2() {
-            return x;
-        }
-
-        public final double inheritedFinalTarget2() {
-            return x;
-        }
-    }
-
-    public class InlinableTargetClass3 extends InlinableTargetClass4 {
-        public double alwaysOverriddenTarget() {
-            return x;
-        }
-
-        public double inheritedTarget3() {
-            return x;
-        }
-
-        public final double inheritedFinalTarget3() {
-            return x;
-        }
-    }
-
-    public class InlinableTargetClass4 {
-        public double alwaysOverriddenTarget() {
-            return x;
-        }
-
-        public double inheritedTarget4() {
-            return x;
-        }
-
-        public final double inheritedFinalTarget4() {
-            return x;
-        }
-    }
-
-    @CompilerControl(DONT_INLINE)
-    public static class TargetClass1 extends TargetClass2 {
-        public void alwaysOverriddenTarget() {
-        }
-
-        public void inheritedTarget1() {
-        }
-
-        public final void inheritedFinalTarget1() {
-        }
-
-        public void targetVirtual() {
-        }
-
-        public final void targetFinal() {
-        }
-    }
-
-    @CompilerControl(DONT_INLINE)
-    public static class TargetClass2 extends TargetClass3 {
-        public void alwaysOverriddenTarget() {
-        }
-
-        public void inheritedTarget2() {
-        }
-
-        public final void inheritedFinalTarget2() {
-        }
-    }
-
-    @CompilerControl(DONT_INLINE)
-    public static class TargetClass3 extends TargetClass {
-        public void alwaysOverriddenTarget() {
-        }
-
-        public void inheritedTarget3() {
-        }
-
-        public final void inheritedFinalTarget3() {
-        }
-    }
-
-    @CompilerControl(DONT_INLINE)
-    public static class TargetClass {
-        public void alwaysOverriddenTarget() {
-        }
-
-        public void inheritedTarget4() {
-        }
-
-        public final void inheritedFinalTarget4() {
-        }
-    }
-
-    public static void main(String[] args) throws RunnerException {
-        Options opt = new OptionsBuilder()
-                .include(".*" + JavaFinalBenchmark.class.getSimpleName() + ".*")
-                .build();
-
-        new Runner(opt).run();
-    }
+    new Runner(opt).run();
+  }
 }
--- src/main/java/com/insightfullogic/java_final_benchmarks/PolymorphicBenchmark.java
+++ src/main/java/com/insightfullogic/java_final_benchmarks/PolymorphicBenchmark.java
@@ -21,171 +21,170 @@ import static org.openjdk.jmh.annotations.Scope.Thread;
 @Fork(5)
 @OutputTimeUnit(NANOSECONDS)
 @State(Thread)
-public class PolymorphicBenchmark
-{
-    // Deliberately a field, JMH avoids constant folding
-    private double x = Math.PI;
+public class PolymorphicBenchmark {
+  // Deliberately a field, JMH avoids constant folding
+  private double x = Math.PI;
 
-    private Polymorph polymorph;
-    private Polymorph childA;
-    private Polymorph childB;
+  private Polymorph polymorph;
+  private Polymorph childA;
+  private Polymorph childB;
 
-    private InlinablePolymorph inlinablePolymorph;
-    private InlinablePolymorph inlinableChildA;
-    private InlinablePolymorph inlinableChildB;
+  private InlinablePolymorph inlinablePolymorph;
+  private InlinablePolymorph inlinableChildA;
+  private InlinablePolymorph inlinableChildB;
 
-    @Setup
-    public void setup() {
-        polymorph = new Polymorph();
-        childA = new OverridingClassA();
-        childB = new OverridingClassB();
+  @Setup
+  public void setup() {
+    polymorph = new Polymorph();
+    childA = new OverridingClassA();
+    childB = new OverridingClassB();
 
-        inlinablePolymorph = new InlinablePolymorph();
-        inlinableChildA = new InlinableOverridingClassA();
-        inlinableChildB = new InlinableOverridingClassB();
-    }
+    inlinablePolymorph = new InlinablePolymorph();
+    inlinableChildA = new InlinableOverridingClassA();
+    inlinableChildB = new InlinableOverridingClassB();
+  }
 
-    @Benchmark
-    public void monomorphicInvoke_warmup() {
-        invoke(polymorph);
-    }
+  @Benchmark
+  public void monomorphicInvoke_warmup() {
+    invoke(polymorph);
+  }
 
-    @Benchmark
-    public void monomorphicInvoke_measure() {
-        invoke(polymorph);
-    }
+  @Benchmark
+  public void monomorphicInvoke_measure() {
+    invoke(polymorph);
+  }
 
-    @Benchmark
-    public void bimorphicInvoke_warmup() {
-        invoke(childA);
-        invoke(childB);
-    }
+  @Benchmark
+  public void bimorphicInvoke_warmup() {
+    invoke(childA);
+    invoke(childB);
+  }
 
-    @Benchmark
-    public void bimorphicInvoke_measure() {
-        invoke(childA);
-    }
+  @Benchmark
+  public void bimorphicInvoke_measure() {
+    invoke(childA);
+  }
 
-    @Benchmark
-    public void megamorphicInvoke_warmup() {
-        invoke(polymorph);
-        invoke(childA);
-        invoke(childB);
-    }
+  @Benchmark
+  public void megamorphicInvoke_warmup() {
+    invoke(polymorph);
+    invoke(childA);
+    invoke(childB);
+  }
 
-    @Benchmark
-    public void megamorphicInvoke_measure() {
-        invoke(childA);
-    }
+  @Benchmark
+  public void megamorphicInvoke_measure() {
+    invoke(childA);
+  }
 
+  @CompilerControl(DONT_INLINE)
+  private void invoke(Polymorph polymorph) {
+    polymorph.polymorphicMethod();
+  }
+
+  @Benchmark
+  public double inlinableMonomorphicInvoke_warmup() {
+    return inlinableInvoke(inlinablePolymorph);
+  }
+
+  @Benchmark
+  public double inlinableMonomorphicInvoke_measure() {
+    return inlinableInvoke(inlinablePolymorph);
+  }
+
+  @Benchmark
+  public double inlinableBimorphicInvoke_warmup() {
+    return inlinableInvoke(inlinableChildA)
+        + inlinableInvoke(inlinableChildB);
+  }
+
+  @Benchmark
+  public double inlinableBimorphicInvoke_measure() {
+    return inlinableInvoke(inlinableChildA);
+  }
+
+  @Benchmark
+  public double inlinableMegamorphicInvoke_warmup() {
+    return inlinableInvoke(inlinablePolymorph)
+        + inlinableInvoke(inlinableChildA)
+        + inlinableInvoke(inlinableChildB);
+  }
+
+  @Benchmark
+  public double inlinableMegamorphicInvoke_measure() {
+    return inlinableInvoke(inlinableChildA);
+  }
+
+  private double inlinableInvoke(InlinablePolymorph polymorph) {
+    return polymorph.polymorphicMethod();
+  }
+
+  public static class Polymorph {
     @CompilerControl(DONT_INLINE)
-    private void invoke(Polymorph polymorph) {
-        polymorph.polymorphicMethod();
+    public void polymorphicMethod() {
     }
+  }
 
-    @Benchmark
-    public double inlinableMonomorphicInvoke_warmup() {
-        return inlinableInvoke(inlinablePolymorph);
+  public static class OverridingClassA extends Polymorph {
+    @CompilerControl(DONT_INLINE)
+    public void polymorphicMethod() {
     }
+  }
 
-    @Benchmark
-    public double inlinableMonomorphicInvoke_measure() {
-        return inlinableInvoke(inlinablePolymorph);
+  public static class OverridingClassB extends Polymorph {
+    @CompilerControl(DONT_INLINE)
+    public void polymorphicMethod() {
     }
+  }
 
-    @Benchmark
-    public double inlinableBimorphicInvoke_warmup() {
-        return inlinableInvoke(inlinableChildA)
-             + inlinableInvoke(inlinableChildB);
+  public class InlinablePolymorph {
+    public double polymorphicMethod() {
+      return x;
     }
+  }
 
-    @Benchmark
-    public double inlinableBimorphicInvoke_measure() {
-        return inlinableInvoke(inlinableChildA);
+  public class InlinableOverridingClassA extends InlinablePolymorph {
+    public double polymorphicMethod() {
+      return x;
     }
+  }
 
-    @Benchmark
-    public double inlinableMegamorphicInvoke_warmup() {
-        return inlinableInvoke(inlinablePolymorph)
-             + inlinableInvoke(inlinableChildA)
-             + inlinableInvoke(inlinableChildB);
+  public class InlinableOverridingClassB extends InlinablePolymorph {
+    public double polymorphicMethod() {
+      return x;
     }
+  }
 
-    @Benchmark
-    public double inlinableMegamorphicInvoke_measure() {
-        return inlinableInvoke(inlinableChildA);
-    }
+  public static void main(String[] args) throws RunnerException {
+    RunResult monomorphResult = makeRunner("monomorphic");
+    RunResult bimorphResult = makeRunner("bimorphic");
+    RunResult megamorphResult = makeRunner("megamorphic");
 
-    private double inlinableInvoke(InlinablePolymorph polymorph) {
-        return polymorph.polymorphicMethod();
-    }
+    RunResult inlinableMonomorphResult = makeRunner("inlinableMonomorphic");
+    RunResult inlinableBimorphResult = makeRunner("inlinableBimorphic");
+    RunResult inlinableMegamorphResult = makeRunner("inlinableMegamorphic");
 
-    public static class Polymorph {
-        @CompilerControl(DONT_INLINE)
-        public void polymorphicMethod() {
-        }
-    }
+    System.out.println("----------------------------------");
+    print("Monomorphic", monomorphResult.getPrimaryResult());
+    print("Bimorphic", bimorphResult.getPrimaryResult());
+    print("Megamorphic", megamorphResult.getPrimaryResult());
+    print("Inlinable Monomorphic", inlinableMonomorphResult.getPrimaryResult());
+    print("Inlinable Bimorphic", inlinableBimorphResult.getPrimaryResult());
+    print("Inlinable Megamorphic", inlinableMegamorphResult.getPrimaryResult());
+    System.out.println("----------------------------------");
+  }
 
-    public static class OverridingClassA extends Polymorph {
-        @CompilerControl(DONT_INLINE)
-        public void polymorphicMethod() {
-        }
-    }
+  private static RunResult makeRunner(String method) throws RunnerException {
+    return new Runner(new OptionsBuilder()
+        .warmupMode(WarmupMode.BULK)
+        .include(".*" + PolymorphicBenchmark.class.getSimpleName() + ".*" + method + ".*_measure")
+        .includeWarmup(".*" + PolymorphicBenchmark.class.getSimpleName() + ".*" + method + ".*_warmup")
+        .build()).runSingle();
+  }
 
-    public static class OverridingClassB extends Polymorph {
-        @CompilerControl(DONT_INLINE)
-        public void polymorphicMethod() {
-        }
-    }
-
-    public class InlinablePolymorph {
-        public double polymorphicMethod() {
-            return x;
-        }
-    }
-
-    public class InlinableOverridingClassA extends InlinablePolymorph {
-        public double polymorphicMethod() {
-            return x;
-        }
-    }
-
-    public class InlinableOverridingClassB extends InlinablePolymorph {
-        public double polymorphicMethod() {
-            return x;
-        }
-    }
-
-    public static void main(String[] args) throws RunnerException {
-        RunResult monomorphResult = makeRunner("monomorphic");
-        RunResult bimorphResult = makeRunner("bimorphic");
-        RunResult megamorphResult = makeRunner("megamorphic");
-
-        RunResult inlinableMonomorphResult = makeRunner("inlinableMonomorphic");
-        RunResult inlinableBimorphResult = makeRunner("inlinableBimorphic");
-        RunResult inlinableMegamorphResult = makeRunner("inlinableMegamorphic");
-
-        System.out.println("----------------------------------");
-        print("Monomorphic", monomorphResult.getPrimaryResult());
-        print("Bimorphic",   bimorphResult.getPrimaryResult());
-        print("Megamorphic", megamorphResult.getPrimaryResult());
-        print("Inlinable Monomorphic", inlinableMonomorphResult.getPrimaryResult());
-        print("Inlinable Bimorphic",   inlinableBimorphResult.getPrimaryResult());
-        print("Inlinable Megamorphic", inlinableMegamorphResult.getPrimaryResult());
-        System.out.println("----------------------------------");
-    }
-
-    private static RunResult makeRunner(String method) throws RunnerException {
-        return new Runner(new OptionsBuilder()
-                .warmupMode(WarmupMode.BULK)
-                .include(".*" + PolymorphicBenchmark.class.getSimpleName() + ".*" + method + ".*_measure")
-                .includeWarmup(".*" + PolymorphicBenchmark.class.getSimpleName() + ".*" + method + ".*_warmup")
-                .build()).runSingle();
-    }
-
-    private static void print(String label, Result res) {
-        double standardDeviation = res.getStatistics().getStandardDeviation();
-        System.out.printf("%5s: %.3f +- %.3f %s%n", label, res.getScore(), standardDeviation, res.getScoreUnit());
-    }
+  private static void print(String label, Result res) {
+    double standardDeviation = res.getStatistics().getStandardDeviation();
+    System.out.printf("%5s: %.3f +- %.3f %s%n", label, res.getScore(), standardDeviation, res.getScoreUnit());
+  }
 
 }
